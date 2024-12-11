df100fb7db13d26dbe4769885986ca399d0a46e3
Lia Medina <liamedina@hotmail.com>
2024-12-11 11:26:22 +0000

    Se agrega funcionalidad

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function sonidoPom () {
-    document.querySelector('#sonido_tecla_pom').play();
+let listaDeTeclas = document.querySelectorAll('.tecla');
+let contador= 0;
+
+function sonidos (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = sonidoPom;
+while (contador < 9){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#sonido_${instrumento}`;
+
+    tecla.onclick = function (){
+        sonidos(idAudio);
+    };
+    contador = contador + 1;
+}
